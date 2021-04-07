b8a2f7fde44f164f07551a91fd2c106c0118dd0b
Brad Grantham <grantham@plunk.org>
2021-04-07 11:41:36 -0700

    RTIOW through section 4

--- main.cpp
+++ main.cpp
@@ -13,21 +13,44 @@ void write_pixel(FILE *fp, const vec3f& color)
     fwrite(rgb8, 3, 1, fp);
 }
 
+vec3f evaluateRay(const ray& r)
+{
+    vec3f dir = vec_normalize(r.m_direction);
+    auto t = 0.5f * (dir.y + 1.0f);
+    return (1.0f - t) * vec3f(1.0f, 1.0f, 1.0f) + t * vec3f(0.5f, 0.7f, 1.0f);
+}
+
 int main(int argc, char **argv)
 {
+    int aspectRatioNum = 16;
+    int aspectRatioDenom = 9;
     int imageWidth = 512;
-    int imageHeight = 512;
+    int imageHeight = imageWidth * aspectRatioDenom / aspectRatioNum;
+
 
     FILE *fp = fopen("image.ppm", "wb");
     fprintf(fp, "P6 %d %d 255\n", imageWidth, imageHeight);
 
+    float viewportHeight = 2.0f;
+    float viewportWidth = viewportHeight * aspectRatioNum / aspectRatioDenom;
+    auto focalLength = 1.0;
+
+    vec3f origin(0, 0, 0);
+    vec3f horizontal(viewportWidth, 0, 0);
+    vec3f vertical(0, viewportHeight, 0);
+    auto lowerLeft = origin - horizontal / 2.0f - vertical / 2.0f - vec3f(0, 0, focalLength);
+
     for(int j = imageHeight - 1; j >= 0; j--) {
         for(int i = 0; i < imageWidth; i++) {
             std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
+
             float u = i * 1.0f / (imageWidth - 1);
             float v = j * 1.0f / (imageHeight - 1);
 
-            vec3f color(u, v, .25f);
+            ray r(origin, lowerLeft + u * horizontal + v * vertical - origin);
+
+            vec3f color = evaluateRay(r);
+
             write_pixel(fp, color);
         }
     }
